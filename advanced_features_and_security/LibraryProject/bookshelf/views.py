5c390c6a0fc19baa7dcc46722a4597b164c36206
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-11-19 14:43:15 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm"]
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.title = request.POST['title']
+        book.author = request.POST['author']
+        book.save()
+        return redirect('book_list')
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+def search_books(request):
+    query = request.GET.get('q', '')
+    books = Book.objects.filter(title__icontains=query) if query else Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
+
+
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, book_id):
